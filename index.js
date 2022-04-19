8dd2fcea1d54247ba26ba9bf0de152f6ea7e7c84
Bikram Kumar <coolbikram0@gmail.com>
2022-04-19 19:28:42 +0530

    added default stylesheets injection in index

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ export {GUIHandler} from "./scripts/GUIHandler.js";
 export {UI_StylesManager} from "./scripts/UI_StylesManager.js";
 export {EventHandler} from "./scripts/EventHandler.js";
 
+import {UI_StylesManager} from "./scripts/UI_StylesManager.js";
+UI_StylesManager.injectAllStyles();
